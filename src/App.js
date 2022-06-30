7203f668996dfe5fde301748f9dcffde20dbeb5e
mindstorm8191 <mindstorm8191@yahoo.com>
2022-06-30 19:33:31 -0400

    First construction
    Putting this project together into a git repository... I already have a few pieces functioning. Hopefully we can go from here

---
+++ src/App.js
@@ -0,0 +1,71 @@
+/*
+        Dark Sun
+    A top-down space shooter with RPG elements
+    Destroy enemy space ships, then salvage parts off the wreckage to improve your own. Salvaged parts don't come whole & working; you must
+    gather components from multiple parts to fabricate a working one. Inner components can then be upgraded to improve its performance
+
+*/
+
+import React from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+// To manage center of gravity, we have a COG object, and everything will attach to that, at its root. We will shift this around as needed to
+// compensate for sides that are heavier than others
+
+function App() {
+    return (
+        <div id="canvas-container">
+            <Canvas style={{ backgroundColor: "#202020", height: 700 }}>
+                <spotLight intensity={0.6} position={[30, 30, 50]} angle={0.2} penumbra={0.5} castShadow />
+                <MyObject />
+            </Canvas>
+        </div>
+    );
+}
+
+function MyObject(props) {
+    const myMesh = React.useRef();
+    const [active, setActive] = React.useState(false);
+    const [clicked, setClicked] = React.useState(false);
+    const [drift, setDrift] = React.useState([0, 0, 0]);
+    let spot = [0, 0, 0];
+
+    useFrame(() => {
+        //Reference.current.rotation.x += 0.005;
+        //Reference.current.rotation.y += 0.005;
+        //console.log("meep!");
+        myMesh.current.rotation.z += 0.005;
+        if (clicked) {
+            //console.log(myMesh.current);
+            myMesh.current.position.x += drift[0];
+            myMesh.current.position.y += drift[1];
+            myMesh.current.position.z += drift[2];
+        }
+    });
+
+    return (
+        <mesh
+            ref={myMesh}
+            scale={active ? 1.5 : 1}
+            position={spot}
+            onPointerEnter={() => setActive(true)}
+            onPointerLeave={() => setActive(false)}
+            onClick={() => {
+                console.log(myMesh.current);
+                setDrift([Math.random() * 0.02 - 0.01, Math.random() * 0.02 - 0.01, Math.random() * 0.02 - 0.01]);
+                setClicked(!clicked);
+            }}
+        >
+            <octahedronGeometry />
+            <meshPhongMaterial color="yellow" />
+            <mesh position={[1.5, 0, 0]}>
+                <boxGeometry />
+                <meshPhongMaterial color="red" />
+            </mesh>
+        </mesh>
+    );
+}
+
+function SpaceFloater(props) {}
+
+export default App;
